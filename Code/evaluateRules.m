71aa18ad2716e69ff5bdcdbd65a3246a02b59472
Mehdi Manshadi <40694321+MehdiManshadi@users.noreply.github.com>
2021-07-07 01:32:02 +0430

    Update evaluateRules.m

--- Code/evaluateRules.m
+++ Code/evaluateRules.m
@@ -1,4 +1,18 @@
 function constrRxnIDs = evaluateRules(model,geneIDs)
+% evaluateRules obtains the affected reactions from GPR rules of COBRA model
+%
+% USAGE:
+%
+%    constrRxnIDs = evaluateRules(model,geneIDs)
+%
+% INPUT:
+%    model:           COBRA model structure contaning "rules" field.
+%
+% OUTPUTS:
+%    constrRxnIDs:    The list of constrained reactions.
+%
+% .. Author:
+%       - Mehdi Dehghan Manshadi 07/2021 based on github.com/RamanLab/FastSL
 rxnInd = find(any(model.rxnGeneMat(:, geneIDs), 2));
 if (~isempty(rxnInd))
     x = true(length(model.genes), 1);
